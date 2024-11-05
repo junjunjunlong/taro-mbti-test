2186f3e672d91fe0ec40f8058206ad4bc70e1e38
zhujl <459310651@qq.com>
2024-11-05 20:37:16 +0800

    init

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,26 @@
+import { View, } from '@tarojs/components'
+import { AtButton, AtTimeline } from 'taro-ui'
+
+import "taro-ui/dist/style/components/button.scss" // 按需引入
+import "taro-ui/dist/style/components/timeline.scss";
+import "taro-ui/dist/style/components/icon.scss";
+
+export default () => {
+  return (
+    <View className='index'>
+      <AtButton loading type='primary'>按钮文案</AtButton>
+      <AtTimeline
+        items={[
+          { title: '刷牙洗脸' },
+          { title: '吃早餐' },
+          { title: '上班' },
+          { title: '睡觉' }
+        ]}
+      >
+      </AtTimeline>
+    </View>
+  )
+
+}
+
+
